a3e457e8f5f1a179495b9de61e31fb0c4e85dc63
robert-fine <rob.fine09@gmail.com>
2022-05-10 20:58:18 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '30em',
+      md: '48em',
+      lg: '61em',
+      xl: '90em'
+    }, 
+    extend: {},
+  },
+  plugins: [],
+}
