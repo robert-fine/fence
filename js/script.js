20d6709a0714777bd66e86870d9e3f4a8880fd93
Robert Fine <robert@robertfine.dev>
2022-05-18 22:51:33 -0500

    added quote form

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 
 const btn = document.getElementById('menu-btn')
 const nav = document.getElementById('menu')
+const quote = document.getElementById('quote-form')
+const quoteBtns = document.querySelectorAll('.get-quote')
+const quoteCloseBtn = document.getElementById('close-quote')
+
 
 btn.addEventListener('click', () => {
   btn.classList.toggle('open')
   nav.classList.toggle('flex')
   nav.classList.toggle('hidden')
 })
+
+quoteBtns.forEach(btn => {
+  btn.addEventListener('click', toggleQuoteForm)
+})
+
+quoteCloseBtn.addEventListener('click', toggleQuoteForm)
+
+function toggleQuoteForm() {
+  quote.classList.toggle('sr-only')
+}
