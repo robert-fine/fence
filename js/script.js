d4acd27e20bfbded73c6705b237bdbd7ef5ded85
robert-fine <rob.fine09@gmail.com>
2022-05-24 22:40:34 -0500

    added gallery

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
-const btn = document.getElementById('menu-btn')
-const nav = document.getElementById('menu')
+const btn = document.getElementById('menu-btn');
+const nav = document.getElementById('menu');
+const galleryItems = document.querySelectorAll('.gallery-item');
 
 
 btn.addEventListener('click', () => {
@@ -8,3 +9,16 @@ btn.addEventListener('click', () => {
   nav.classList.toggle('flex')
   nav.classList.toggle('hidden')
 })
+
+galleryItems.forEach(item => {
+  item.addEventListener('click', () => {
+    removeMainTag()
+    item.classList.add('gallery-item-main')
+  })
+})
+
+function removeMainTag() {
+  galleryItems.forEach(item => {
+    item.classList.remove('gallery-item-main')
+  })
+}
