ab9b0da15741d07f70d39fd292429f2ed527a269
Robert Fine <robert@robertfine.dev>
2022-05-31 22:01:50 -0500

    added return to top button

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 const btn = document.getElementById('menu-btn');
 const nav = document.getElementById('menu');
 const galleryItems = document.querySelectorAll('.gallery-item');
+const toTopBtn = document.getElementById('toTopBtn');
 
 
 btn.addEventListener('click', () => {
@@ -22,3 +23,18 @@ function removeMainTag() {
     item.classList.remove('gallery-item-main')
   })
 }
+
+toTopBtn.addEventListener('click', ()=> {
+  window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+});
+
+window.addEventListener('scroll', ()=> {
+  if (window.pageYOffset > 200) {
+    // toTopBtn.classList.add('opacity-1')
+    toTopBtn.classList.remove('opacity-0')
+  } else if (window.pageYOffset < 125) {
+    toTopBtn.classList.add('opacity-0')
+  }
+
+  console.log(window.pageYOffset)
+})
